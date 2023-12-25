6d74124b7f919a9e1f69aa7e29fff4603ff4d584
MittaUshasri <143583661+MittaUshasri@users.noreply.github.com>
2023-12-25 12:24:51 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 export const fetchResponse =  async(chat) => {
     try {
         // after depoloyment you should change the fetch URL below
-        const response = await fetch('https://chatbot-noat.onrender.com/', { 
+        const response = await fetch('https://chatbot-noat.onrender.com', { 
             method: 'POST',
             headers: {
                 "Content-Type": "application/json"
